01684c0b3c4c75658e9c1f11e73a96d50933e197
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-02-16 21:14:58 -0600

    Bare minimum search option.

---
+++ equajson.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import os
+import sys
+import json
+
+def pretty_print(equation):
+    print(equation["description"]["terse"])
+    eqn_dict = equation["unicode-pretty-print"]
+    equation_text = eqn_dict["multiline"]
+    for line in equation_text:
+        print(line)
+    if "parameters" in eqn_dict:
+        print("where:")
+        for param, param_dict in eqn_dict["parameters"].iteritems():
+            label = param_dict["label"]
+            print(param,'=',label)
+
+
+def main(query):
+    here = sys.path[0]
+    json_dir = os.path.join(here, 'equajson')
+
+    for filename in os.listdir(json_dir):
+        if not filename.endswith('.json'):
+            continue
+        filepath = os.path.join(json_dir, filename)
+        with open(filepath) as json_file:
+            try:
+                equation = json.load(json_file)
+            except ValueError:
+                sys.stderr.write("Invalid JSON for file: `{}'\n".format(json_file.name))
+                continue # try the next file
+            description = equation["description"]["verbose"]
+            if query.lower() in description.lower():
+                pretty_print(equation)
+
+
+if __name__ == '__main__':
+    num_args = len(sys.argv) - 1
+    if num_args != 1:
+        sys.stderr.write("Usage: python "+sys.argv[0]+" query"+'\n')
+        sys.exit(1)
+    main(sys.argv[1])
