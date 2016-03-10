b352928633cbb26e22443ff7f782245ae44ec889
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-03-10 13:14:00 -0600

    Look before leaping.

--- ad-hoc-scripts/lift.py
+++ ad-hoc-scripts/lift.py
@@ -15,16 +15,19 @@ for arg in sys.argv[1:]:
         "fixed-constant",
         "special-function"
     ]
-    for term in equajson["unicode-pretty-print"]["terms"]:
-        if any(x in field_list for x in term.keys()):
-            term["classification"] = {}
-            del_list = []
-            for field in term.keys():
-                if field in field_list:
-                    term["classification"][field] = term[field]
-                    del_list.append(field)
-            for field in del_list:
-                del term[field]
+    try:
+        for term in equajson["unicode-pretty-print"]["terms"]:
+            if any(x in field_list for x in term.keys()):
+                term["classification"] = {}
+                del_list = []
+                for field in term.keys():
+                    if field in field_list:
+                        term["classification"][field] = term[field]
+                        del_list.append(field)
+                for field in del_list:
+                    del term[field]
+    except KeyError:
+        pass
 
     with open(sys.argv[1], 'w') as f:
         json.dump(equajson, f, indent=4, separators=(',', ': '), ensure_ascii=False, sort_keys=True)
