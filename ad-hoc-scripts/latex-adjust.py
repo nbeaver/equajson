f5dd47c46b7c9c3ab5a73dec7d4ae8669f4c845d
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-05-11 19:23:37 -0500

    Planning for mass conversion of LaTeX output to standalone documents.

---
+++ ad-hoc-scripts/latex-adjust.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import sys
+import json
+
+for arg in sys.argv[1:]:
+    with open(arg) as f:
+        equajson = json.load(f)
+
+    latex = equajson["markup-languages"]["LaTeX"][0]["markup"]
+
+    if 'documentclass' not in latex:
+        with_boilerplate = "\\documentclass{article}\n\\begin{document}\n\\[\n%s\n\\]\n\\end{document}" % latex
+        equajson["markup-languages"]["LaTeX"][0]["markup"] = with_boilerplate
+
+    with open(arg, 'w') as f:
+        json.dump(equajson, f, indent=4, separators=(',', ': '), ensure_ascii=False, sort_keys=True)
