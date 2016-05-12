ee721abb8acdf4a23e1e857a4af55a1857342e47
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-05-11 19:37:44 -0500

    Catch missing keys.

--- ad-hoc-scripts/latex-adjust.py
+++ ad-hoc-scripts/latex-adjust.py
@@ -7,7 +7,10 @@ for arg in sys.argv[1:]:
     with open(arg) as f:
         equajson = json.load(f)
 
-    latex = equajson["markup-languages"]["LaTeX"][0]["markup"]
+    try:
+        latex = equajson["markup-languages"]["LaTeX"][0]["markup"]
+    except KeyError:
+        continue
 
     if 'documentclass' not in latex:
         with_boilerplate = "\\documentclass{article}\n\\begin{document}\n\\[\n%s\n\\]\n\\end{document}" % latex
