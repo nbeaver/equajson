65cb0a8da45ca4215840fc2e243ecb443a668d69
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2020-08-06 20:37:35 -0400

    Only do this if standalone.

--- new_equation.py
+++ new_equation.py
@@ -7,10 +7,6 @@ import sys
 import json
 import uuid
 
-if len(sys.argv) > 1:
-    sys.stderr.write("Usage: python "+sys.argv[0]+" 'command-invocation'"+'\n')
-    sys.exit(1)
-
 def get_year():
     now = datetime.datetime.now()
     return now.year
@@ -30,77 +26,82 @@ def get_username():
         import getpass
         return getpass.getuser()
 
-new_uuid = str(uuid.uuid4())
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        sys.stderr.write("Usage: python "+sys.argv[0]+" 'command-invocation'"+'\n')
+        sys.exit(1)
 
-equajson = \
-{
-    "copying": {
-        "authors": [
-            get_username()
-        ],
-        "license-name": "MIT (Expat) License",
-        "license-url": "http://opensource.org/licenses/MIT",
-        "year": get_year(),
-    },
-    "description": {
-        "verbose": "<FIXME>",
-        "terse": "<FIXME>",
-    },
-    "markup-languages": {
-        "LaTeX": [
-            {
-                "markup": "<FIXME>",
-            }
-        ]
-    },
-    "relevant-urls": [
-        {
-            "date-known-good": get_iso_date(),
-            "url": "<FIXME>",
-        }
-    ],
-    "unicode-pretty-print": {
-        "multiline": [
-            "<FIXME>",
-            "<FIXME>",
-            "<FIXME>",
-        ],
-        "terms": [
-            {
-                "classification": {
-                    "always-an-integer": False,
-                    "always-dimensionless": False,
-                    "always-positive": False,
-                    "always-scalar": False,
-                    "bound-variable": False,
-                    "fixed-constant": False,
-                    "special-function": False,
-                },
-                "label": "<FIXME>",
-                "symbol": "<FIXME>",
-                "urls": [
-                    "<FIXME>",
-                ]
-            },
-            {
-                "classification": {
-                    "always-dimensionless": False,
-                    "fixed-constant": False,
-                },
-                "label": "<FIXME>",
-                "symbol": "<FIXME>",
-                "urls": [
-                    "<FIXME>",
-                ]
-            }
-        ]
-    },
-    "uuid": new_uuid,
-}
+    new_uuid = str(uuid.uuid4())
 
-root = sys.path[0]
-new_filename = new_uuid + '.json'
-new_filepath = os.path.join(root, 'json', new_filename)
-with open(new_filepath, 'w') as new_file:
-    json.dump(equajson, new_file, indent=4, separators=(',', ': '), sort_keys=True)
-    print('Created new equation:\n{}'.format(new_filepath))
+    equajson = \
+    {
+        "copying": {
+            "authors": [
+                get_username()
+            ],
+            "license-name": "MIT (Expat) License",
+            "license-url": "http://opensource.org/licenses/MIT",
+            "year": get_year(),
+        },
+        "description": {
+            "verbose": "<FIXME>",
+            "terse": "<FIXME>",
+        },
+        "markup-languages": {
+            "LaTeX": [
+                {
+                    "markup": "<FIXME>",
+                }
+            ]
+        },
+        "relevant-urls": [
+            {
+                "date-known-good": get_iso_date(),
+                "url": "<FIXME>",
+            }
+        ],
+        "unicode-pretty-print": {
+            "multiline": [
+                "<FIXME>",
+                "<FIXME>",
+                "<FIXME>",
+            ],
+            "terms": [
+                {
+                    "classification": {
+                        "always-an-integer": False,
+                        "always-dimensionless": False,
+                        "always-positive": False,
+                        "always-scalar": False,
+                        "bound-variable": False,
+                        "fixed-constant": False,
+                        "special-function": False,
+                    },
+                    "label": "<FIXME>",
+                    "symbol": "<FIXME>",
+                    "urls": [
+                        "<FIXME>",
+                    ]
+                },
+                {
+                    "classification": {
+                        "always-dimensionless": False,
+                        "fixed-constant": False,
+                    },
+                    "label": "<FIXME>",
+                    "symbol": "<FIXME>",
+                    "urls": [
+                        "<FIXME>",
+                    ]
+                }
+            ]
+        },
+        "uuid": new_uuid,
+    }
+
+    root = sys.path[0]
+    new_filename = new_uuid + '.json'
+    new_filepath = os.path.join(root, 'json', new_filename)
+    with open(new_filepath, 'w') as new_file:
+        json.dump(equajson, new_file, indent=4, separators=(',', ': '), sort_keys=True)
+        print('Created new equation:\n{}'.format(new_filepath))
