81a49c80e1c0f1dc80ca84024d20ad809fa2bf0c
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2020-08-10 23:19:09 -0400

    Add logging.

--- validate_equajson.py
+++ validate_equajson.py
@@ -4,17 +4,18 @@ import jsonschema
 import sys
 import os
 import argparse
+import logging
 
 def validate_single_file(json_fp, schema):
     try:
         equajson = json.load(json_fp)
     except:
-        sys.stderr.write("Invalid JSON in file: `"+json_fp.name+"'"+'\n')
+        logging.error("Invalid JSON in file: {}".format(json_fp.name))
         raise
     try:
         jsonschema.validate(equajson, schema)
     except jsonschema.exceptions.ValidationError:
-        sys.stderr.write(json_fp.name+'\n')
+        logging.error("in file: {}".format(json_fp.name))
         raise
     basename_no_extension = os.path.splitext(os.path.basename(json_fp.name))[0]
 
@@ -30,7 +31,25 @@ def main():
         'json_file',
         help='path to json file to validate'
     )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        help='More verbose logging',
+        dest="loglevel",
+        default=logging.WARNING,
+        action="store_const",
+        const=logging.INFO,
+    )
+    parser.add_argument(
+        '-d',
+        '--debug',
+        help='Enable debugging logs',
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+    )
     args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
 
     equajson_path = args.json_file
     schema_path = args.schema
@@ -39,7 +58,7 @@ def main():
             try:
                 equajson_schema = json.load(schema_file)
             except:
-                sys.stderr.write("Invalid JSON in schema: `"+schema_file.name+"'"+'\n')
+                logging.error("in file: {}".format(schema_path))
                 raise
 
     with open(equajson_path) as json_file:
