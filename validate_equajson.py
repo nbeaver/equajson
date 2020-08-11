44cdeb2ebfe96fe2cb59188d2f7267d600040dd3
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2020-08-10 23:12:20 -0400

    Get rid of unnecessary global variables.

--- validate_equajson.py
+++ validate_equajson.py
@@ -5,11 +5,22 @@ import sys
 import os
 import argparse
 
-def main(equajson_path, schema_path):
+def main():
+    parser = argparse.ArgumentParser(description='validate equajson files')
+    parser.add_argument(
+        '-s',
+        '--schema',
+        help='path to schema file',
+        required=True
+    )
+    parser.add_argument(
+        'json_file',
+        help='path to json file to validate'
+    )
+    args = parser.parse_args()
 
-    global filepath
-
-    filepath = equajson_path
+    equajson_path = args.json_file
+    schema_path = args.schema
 
     with open(schema_path) as schema_file:
             try:
@@ -31,21 +42,5 @@ def main(equajson_path, schema_path):
                 raise
             basename_no_extension = os.path.splitext(os.path.basename(json_file.name))[0]
 
-# It's easier to make this a global variable
-# than to thread it through every function.
-filepath = None
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='validate equajson files')
-    parser.add_argument(
-        '-s',
-        '--schema',
-        help='path to schema file',
-        required=True
-    )
-    parser.add_argument(
-        'json_file',
-        help='path to json file to validate'
-    )
-    args = parser.parse_args()
-    main(args.json_file, args.schema)
+    main()
